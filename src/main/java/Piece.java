5a6e4b3eee36ed7b7b735193899f567a73c2b02a
LeonGIA <lonphi12@gmail.com>
2022-04-06 16:40:07 -0400

    Fixed test case error by adding abstract method in Piece

--- src/main/java/Piece.java
+++ src/main/java/Piece.java
@@ -16,12 +16,16 @@ public abstract class Piece {
     protected String teamColor;
     protected boolean hidden ;
     protected boolean original;
+
     public Piece(char symbol, String teamColor, boolean hidden, boolean original){
         this.symbol = symbol;
         this.teamColor = teamColor;
         this.hidden = hidden;
         this.original = original;
     }
+
+    public abstract void speak();
+
     @Override
     public String toString(){
         return teamColor + " " + symbol;
