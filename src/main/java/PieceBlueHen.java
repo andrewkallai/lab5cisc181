ed1d6fa9d703eaccaf4bed49fce59902386956c4
LeonGIA <lonphi12@gmail.com>
2022-04-06 13:52:34 -0400

    test merge conflict

--- src/main/java/PieceBlueHen.java
+++ src/main/java/PieceBlueHen.java
@@ -106,4 +106,9 @@ public class PieceBlueHen extends Piece{
     public boolean canSpawn(){
         return true;
     }
+
+    public static void main(String args[])
+    {
+        System.out.println("Hi");
+    }
 }
