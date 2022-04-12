f2de649c89abbb604d84efe86a3a9b499933c4a7
Andrew Kallai <andrewka@udel.edu>
2022-04-11 20:34:53 -0400

    Finished boardsquare class and all tests passed.

--- src/main/java/BoardSquare.java
+++ src/main/java/BoardSquare.java
@@ -1,19 +1,5 @@
 public class BoardSquare {
-    /*
-    Create a class named BoardSquare with the following:
-property to represent whether the space is empty or not  (true/false)
-property of type Piece to represent the Piece that is located in this square
-property to represent color of the board square(string)
-constructor that accepts the color and sets this property – the square should not have a Piece by default (empty)
-accessors - getPiece, isEmpty, getSquareColor
-mutator – setPiece which accepts a Piece and sets the Piece member field and updates the empty property
-mutator – removePiece which has no parameters and returns the Piece that is on this square, this method must update the Piece to null and empty member fields  (Note: Think about how you can return the Piece while also setting the piece property to null)
-toString() method – accepts no parameters, returns a String
-if no Piece on this square should return:  "-------" (7 dashes)
-if there is a Piece on this square should return: "-"  followed by the Piece’s toString() followed by "-"
-Create a new test file the same way you have for all your previous labs. Read the test cases in BoardSquareTest.java file on Canvas. Copy and paste these test cases to test your code.
-     */
-    private boolean isEmpty;
+    private boolean isEmpty = true;
     private Piece representPiece;
     private String color;
     public BoardSquare(String color){
@@ -32,14 +18,17 @@ Create a new test file the same way you have for all your previous labs. Read th
         representPiece = gamePiece;
         isEmpty = false;
     }
-    /*
-    public Piece removePiece{
-        return setPiece()
+
+    public Piece removePiece(){
+        Piece holdReturn = representPiece;
+        representPiece = null;
+        isEmpty = true;
+        return holdReturn;
     }
-     */
+
     @Override
     public String toString(){
-        if (representPiece == NullType){
+        if (representPiece == null){
             return "-------";
         }
         else{
